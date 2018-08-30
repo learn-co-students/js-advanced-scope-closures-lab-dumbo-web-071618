139e20784de3b374e22d08158593f2025842a15c
github username <github email address>
2018-08-29 23:25:07 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+const produceDrivingRange = (blockRange) => (st1, st2) => {
+    let differenece = Math.abs(parseInt(st1) - parseInt(st2))
+    if (differenece > blockRange) {
+        return `${differenece - blockRange} blocks out of range`
+    } else {
+        return `within range by ${blockRange - differenece}`
+    }
+}
+
+const produceTipCalculator = (percentage) => (total) => percentage * total
